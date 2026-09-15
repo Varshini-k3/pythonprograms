fa51360ff30a2f2468b6634a8fad1198ac1a6bb0
varshini-v6 <varshinishiremath6@gmail.com>
2026-09-15 21:36:10 +0530

    Add files via upload

---
+++ arthematicprogram.py
@@ -0,0 +1,15 @@
+#the program to use arthematic operations on two numbers using variables
+
+a = 10        #declaring a variable
+b = 3         #declaring a second  variable
+
+#use arthematic operations
+
+print(a + b)      #addtion operation
+print(a - b)      #substraction operation
+print(a * b)      #multiplication operation
+print(a / b)      #division
+print(a // b)     #division roundoff method
+print(a ** b)     #power 
+
+
---
+++ swap.py
@@ -0,0 +1,11 @@
+#swaping two numbers using 3rd variables
+
+a = 10       #a variables has been initialized
+b = 20
+print("Before Swapping numbers:",a,b)
+
+temp = a
+a = b
+b = temp
+
+print("Swapped numbers:",a,b)
