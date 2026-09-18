a81fbd91bce428291622b982f7aac8e0a3bf668c
varshini-v6 <varshinishiremath6@gmail.com>
2026-09-18 22:27:32 +0530

    Add files via upload

---
+++ simplecalculator.py
@@ -0,0 +1,10 @@
+# 1. Pick your two numbers
+number_one = 10
+number_two = 5
+
+# 2. Multiply them together
+result = number_one * number_two
+
+# 3. Show the answer on the screen
+print("The answer is:")
+print(result)
