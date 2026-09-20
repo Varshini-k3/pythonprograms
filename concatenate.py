45bcec1d21558c35012c3a9c966d780be82c2e80
varshini-v6 <varshinishiremath6@gmail.com>
2026-09-20 22:02:40 +0530

    Add files via upload

---
+++ concatenate.py
@@ -0,0 +1,8 @@
+#program to execute concatination of two strings 
+str1 = input("Enter a srting1")
+str2 = input("Enter a srting2")
+
+# Joining strings with a space in between
+result = str1 + " " + str2
+print(result)  
+
